ac925c03577734210a434e1f32032bb89d3c0cd9
denysnovoascmspain <denys.novoa@scmspain.com>
2017-04-24 13:12:50 +0200

    annotations

---
+++ src/annotations/MainAnnotations.java
@@ -0,0 +1,12 @@
+package annotations;
+
+public class MainAnnotations {
+
+  public static void main(String[] args) {
+
+    @Preamble(author = "Denys"  )
+    int totalItems;
+
+
+  }
+}
---
+++ src/annotations/Preamble.java
@@ -0,0 +1,5 @@
+package annotations;
+
+public @interface Preamble {
+  String author() default "";
+}
